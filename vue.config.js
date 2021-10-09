2b0d5878a2186fcc91a989b07c7a942befe946b1
kwqlymtctzl <kwqlymtctzl@126.com>
2021-10-10 07:41:30 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -37,9 +37,10 @@ const path = require('path');        //引入path模块
 function resolve(dir) {
     return path.join(__dirname, dir)  //path.join(__dirname)设置绝对路径
 }
-const BASE_URL = process.env.NODE_ENV === "production" ? "/mall/" : "/";
+// const BASE_URL = process.env.NODE_ENV === "production" ? "/mall/" : "/";
 module.exports = {
-    publicPath: BASE_URL,
+    // publicPath: BASE_URL,
+    baseUrl: isProd ? '/mall/' : '/',
     chainWebpack: (config) => {
         config.resolve.alias
             //set第一个参数：设置的别名，第二个参数：设置的路径
