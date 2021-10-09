eed86cf767485ec188e7d640b689d54517e12a68
kwqlymtctzl <kwqlymtctzl@126.com>
2021-10-10 06:43:29 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -46,7 +46,17 @@ module.exports = {
             .set('components', resolve('./src/components'))
             .set('views', resolve('./src/views'))
             .set('common', resolve('./src/common'))
-    }
+    },
+    devServer: {
+        port: 8080, // 设置端口号
+        host: '192.168.177.106', // ip
+        disableHostCheck: true, //是否关闭用于 DNS 重绑定的 HTTP 请求的 HOST 检查
+        hotOnly: false, // 热更新
+        https: false, // https:{type:Boolean}配置前缀
+        open: false, //配置自动启动浏览器
+        proxy: null,  //设置代理
+    },
+    // publicPath: ''
 }
 
 // 对于javascript引入：
